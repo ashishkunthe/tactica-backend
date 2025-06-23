90f071f0b293ddb255e9567345b2855756c3a37c
ashishkunthe <ashishkunthe2003@gmail.com>
2025-06-23 16:51:59 +0530

    added the auth middleware

---
+++ src/middleware/authMiddleware.ts
@@ -0,0 +1,36 @@
+import { NextFunction, Request, Response } from "express";
+import jwt from "jsonwebtoken";
+
+const jwt_secret = process.env.JWT_SECRET as string;
+
+interface AuthenticatedRequest extends Request {
+  userId?: string;
+}
+
+function authMiddleware(
+  req: AuthenticatedRequest,
+  res: Response,
+  next: NextFunction
+) {
+  const token = req.headers.authorization;
+
+  try {
+    // @ts-ignore
+    const decode = jwt.verify(token, jwt_secret) as { userId: string };
+    if (!decode) {
+      return res.json({
+        message: "incorrect credentials",
+      });
+    }
+
+    req.userId = decode.userId;
+
+    next();
+  } catch (error) {
+    res.status(401).json({
+      message: " something went wrong",
+    });
+  }
+}
+
+export default authMiddleware;
