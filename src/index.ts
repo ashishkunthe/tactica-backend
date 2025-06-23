26f35922facf33aafd779c5e3a633dabc665b7c5
ashishkunthe <ashishkunthe2003@gmail.com>
2025-06-23 12:59:52 +0530

    docker file and basic code template

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,20 @@
-console.log("hi there");
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.json("hi there server is running");
+});
+
+const PORT = process.env.PORT;
+
+app.listen(PORT, () => {
+  console.log(`serving at PORT ${PORT}`);
+});
